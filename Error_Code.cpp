8b29d8bd310efafe832a710ee615f0f60777fce9
rnd.07 <rnd.07@korosispecindo.com>
2023-03-03 22:52:04 +0700

    fourth commit

---
+++ Error_Code.cpp
@@ -0,0 +1,71 @@
+//============================================================================
+// Name        : Error_Code.cpp
+// Author      : Abdurrahman Nurhakim
+// Version     : 1.1.2
+// Copyright   : Your copyright notice
+// Description : Error Code with Flexible Input Parameter (LinkedList Based)
+//============================================================================
+
+#include "Error_Code.h"
+
+ErrorCodeClass::ErrorCodeClass() {
+}
+
+ErrorCode* ErrorCodeClass::createErrorCodeList(int parameters[]) {
+	ErrorCode *head = NULL;
+	ErrorCode *curr = NULL;
+	my_typedata_16_t Value_16_bit;
+
+	for (int i = 0; i < SIX_TEEN_BIT; i++) {
+		Value_16_bit |= (parameters[i] << i);
+		ErrorCode *node = new ErrorCode { Value_16_bit, parameters[i], NULL };
+		if (head == NULL) {
+			head = node;
+			curr = node;
+		} else {
+			curr->next = node;
+			curr = node;
+		}
+	}
+	return head;
+}
+
+void ErrorCodeClass::ReadErrorCode(ErrorCode *head,
+		my_typedata_16_t *OutputHEXValue) {
+	ErrorCode *curr = head;
+	while (curr != NULL) {
+		*OutputHEXValue = curr->hexValue;
+		curr = curr->next;
+	}
+}
+
+void ErrorCodeClass::deleteErrorCodeList(ErrorCode *head) {
+	ErrorCode *curr = head;
+
+	while (curr != NULL) {
+		ErrorCode *temp = curr;
+		curr = curr->next;
+		delete temp;
+	}
+}
+
+/**
+ example testing:
+ **/
+
+/**
+
+ uint8_t readDEcimal;
+ int readBit;
+ int parameters[] = {1, 1, 1, 0, 0, 1, 0, 0};
+ ErrorCodeClass ErrorCode;
+ int main() {
+
+ struct ErrorCode* head = createErrorCodeList(parameters);
+ ErrorCode.ReadErrorCode(head, &readHEX);
+ ErrorCode.deleteErrorCodeList(head); //free
+ printf("%d", readHEX);
+ return 0;
+ }
+
+ **/
---
+++ Error_Code.h
@@ -0,0 +1,46 @@
+//============================================================================
+// Name        : Error_Code.h
+// Author      : Abdurrahman Nurhakim
+// Version     : 1.1.2
+// Copyright   : Your copyright notice
+// Description : Error Code with Flexible Input Parameter (LinkedList Based)
+//============================================================================
+
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <bits/types.h>
+#include "unit-test.h"
+
+typedef unsigned short int	my_typedata_16_t; ///my type data 16 bit
+
+typedef enum {
+	NULL_BIT,
+	FOUR_BIT = 4,
+	EIGHT_BIT = 8,
+	SIX_TEEN_BIT = 16,
+	THIRTY_TWO_BIT = 32,
+	SIXTY_FOUR_BIT = 64
+};
+
+struct ErrorCode {
+	my_typedata_16_t hexValue;
+	int isError;
+    ErrorCode* next;
+};
+
+class ErrorCodeClass {
+public:
+	ErrorCodeClass();
+	ErrorCode* createErrorCodeList(int parameters[]);
+	void ReadErrorCode(ErrorCode* head, my_typedata_16_t *OutputValue);
+	void deleteErrorCodeList(ErrorCode* head); ///it seems like free() in linkedlist
+};
+
+
